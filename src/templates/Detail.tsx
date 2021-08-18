91fa565871bd1bcb9bc41b3b0590a9ee96b1ceb2
Ryan <rzimmerman@talusanalytics.com>
2021-08-17 19:52:30 -0600

    Making the Detail template actually generate unique pages for each entry

--- src/templates/Detail.tsx
+++ src/templates/Detail.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Main from '../components/layout/Main'
+import NavBar from '../components/layout/NavBar/NavBar'
+import FigmaProvider from '../figma/FigmaProvider'
 
-const Detail = props => {
-  console.log(props)
-  return <p>detail page</p>
+interface PageContext {
+  data: {
+    Description: string
+    Key_Topic_Areas_s_: string[]
+    Resource_Name: string
+    Short_Name: string
+  }
+}
+
+const Detail = ({ pageContext: { data } }: { pageContext: PageContext }) => {
+  console.log(data)
+  return (
+    <FigmaProvider>
+      <NavBar />
+      <Main>
+        <h1>{data.Resource_Name}</h1>
+        <p>{data.Description}</p>
+      </Main>
+    </FigmaProvider>
+  )
 }
 
 export default Detail
