98586b47699755dc776f194bd7a6c68f67a351f6
Ryan <rzimmerman@talusanalytics.com>
2021-08-18 19:50:17 -0600

    Adding placeholder component for the thumbnail

---
+++ src/components/detailpage/Thumbnail/Thumbnail.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ThumbnailContainer = styled.div`
+  grid-area: thumbnail;
+  height: 150px;
+  padding-top: 30px;
+`
+
+const Thumbnail = () => <ThumbnailContainer>Thumbnail here</ThumbnailContainer>
+
+export default Thumbnail
