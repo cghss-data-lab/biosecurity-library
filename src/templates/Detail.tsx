b88956b9e339c97947d4f9a707aaebab3e661656
Ryan <rzimmerman@talusanalytics.com>
2021-08-18 19:52:39 -0600

    Changing detail page to use new grid layout, thumbnail placeholder, and header component

--- src/templates/Detail.tsx
+++ src/templates/Detail.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Breadcrumbs from '../components/detailpage/Breadcrumbs/Breadcrumbs'
+import Grid from '../components/detailpage/Grid'
+import Header from '../components/detailpage/Header/Header'
+import Thumbnail from '../components/detailpage/Thumbnail/Thumbnail'
 import Main from '../components/layout/Main'
 import NavBar from '../components/layout/NavBar/NavBar'
 import FigmaProvider from '../figma/FigmaProvider'
@@ -10,21 +13,22 @@ export interface PageContext {
     Key_Topic_Areas_s_: string[]
     Resource_Name: string
     Short_Name: string
+    Resource_Type: string
+    Authoring_Organization: string
   }
 }
 
-const Detail = ({ pageContext: { data } }: { pageContext: PageContext }) => {
-  console.log(data)
-  return (
-    <FigmaProvider>
-      <NavBar />
-      <Main>
-        <Breadcrumbs {...{ data }} />
-        <h1>{data.Resource_Name}</h1>
-        <p>{data.Short_Description}</p>
-      </Main>
-    </FigmaProvider>
-  )
-}
+const Detail = ({ pageContext: { data } }: { pageContext: PageContext }) => (
+  <FigmaProvider>
+    <NavBar />
+    <Main>
+      <Breadcrumbs {...{ data }} />
+      <Grid>
+        <Thumbnail />
+        <Header {...{ data }} />
+      </Grid>
+    </Main>
+  </FigmaProvider>
+)
 
 export default Detail
