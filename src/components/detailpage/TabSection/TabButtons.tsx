eb7c6415e80340746b2532f8806c1934b6118a28
Ryan <rzimmerman@talusanalytics.com>
2021-08-20 13:26:59 -0600

    Adding column of tab buttons down the side of the page

---
+++ src/components/detailpage/TabSection/TabButtons.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled, { StyledProps } from 'styled-components'
+
+import { Tab } from './TabSection'
+
+const ButtonColumn = styled.section`
+  grid-area: tabs;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  border-top: none;
+`
+
+interface ButtonProps {
+  readonly active: boolean
+}
+
+const Button = styled.button<ButtonProps>`
+  background: none;
+  border: none;
+  border-top: 1px solid ${({ theme }) => theme.colorMedGray};
+  border-left: 1px solid ${({ theme }) => theme.colorMedGray};
+  padding: 17px 20px;
+  text-align: left;
+
+  &:last-of-type {
+    border-bottom: 1px solid ${({ theme }) => theme.colorMedGray};
+  }
+
+  ${({ active, theme }) =>
+    active &&
+    `
+    background: ${theme.colorVeryLightGray};
+    font-weight: 600;
+  `}
+`
+
+interface TabButtonsProps {
+  tabs: Tab[]
+  activeTab: Tab
+  setActiveTab: React.Dispatch<React.SetStateAction<Tab>>
+}
+
+const TabButtons: React.FC<TabButtonsProps> = ({
+  tabs,
+  activeTab,
+  setActiveTab,
+}) => {
+  return (
+    <ButtonColumn>
+      {tabs.map(tab => (
+        <Button
+          key={tab.id}
+          active={activeTab.id === tab.id}
+          onClick={() => setActiveTab(tab)}
+        >
+          {tab.label}
+        </Button>
+      ))}
+    </ButtonColumn>
+  )
+}
+
+export default TabButtons
