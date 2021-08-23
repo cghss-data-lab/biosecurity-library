26f13053658eda061ae758b62363760efe549a46
Ryan <rzimmerman@talusanalytics.com>
2021-08-23 16:39:54 -0600

    Renaming this variable so it makes more sense

--- src/airtable-cms/getCMSText.ts
+++ src/airtable-cms/getCMSText.ts
@@ -1,6 +1,6 @@
 import { AirtableCMSText } from './types'
 
-const getCMSText = (homePageText: AirtableCMSText, name: string) =>
-  homePageText.nodes.find(n => n.data.Name === name)?.data.Text
+const getCMSText = (data: AirtableCMSText, name: string) =>
+  data.nodes.find(n => n.data.Name === name)?.data.Text
 
 export default getCMSText
