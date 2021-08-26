70f8097748ae664e2436d8c6234bc93672d3b55d
Ryan <rzimmerman@talusanalytics.com>
2021-08-26 10:06:50 -0600

    Adding the typeahead control to the filter so things are selectable

--- src/components/explorepage/FilterBar/filters/KeyTopic.tsx
+++ src/components/explorepage/FilterBar/filters/KeyTopic.tsx
@@ -4,12 +4,21 @@ import { useStaticQuery, graphql } from 'gatsby'
 
 import { FilterOptions, FilterProps } from '../FilterBar'
 
+// @ts-ignore: implicit any
+import TypeaheadControl from '../../../ui/TypeaheadControl/TypeaheadControl'
+// @ts-ignore: implicit any
+import TypeaheadResult from '../../../ui/TypeaheadControl/TypeaheadResult'
+
+import { useState } from 'react'
+
 const FilterLabel = styled.label`
+  flex: 1 1 0;
   display: flex;
   flex-direction: column;
 `
 const NameContainer = styled.div`
   display: flex;
+  justify-content: space-between;
 `
 
 const KeyTopic: React.FC<FilterProps> = ({ filters, setFilters }) => {
@@ -27,10 +36,33 @@ const KeyTopic: React.FC<FilterProps> = ({ filters, setFilters }) => {
       }
     }
   `)
-  console.log(options)
+
+  interface Selected {
+    label: string
+  }
+  const [selectedOptions, setSelectedOptions] = useState<Selected>()
+
   return (
     <FilterLabel>
-      <NameContainer>Key Topic Filter</NameContainer>
+      <NameContainer>
+        <div>Key Topic Filter</div>
+        <button>ex</button>
+      </NameContainer>
+      <TypeaheadControl
+        className={''}
+        placeholder={selectedOptions?.label || 'key topics'}
+        items={options.map(o => ({ label: o }))}
+        value={selectedOptions}
+        onChange={(option: any) => {
+          if (option) {
+            setSelectedOptions(option)
+            console.log(option)
+          }
+        }}
+        renderItem={({ label }: { label: string }) => (
+          <TypeaheadResult>{label}</TypeaheadResult>
+        )}
+      />
     </FilterLabel>
   )
 }
