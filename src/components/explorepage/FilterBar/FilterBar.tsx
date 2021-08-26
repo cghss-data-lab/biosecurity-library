391260244bf26b5511b3c4d7f32e33186008c45d
Ryan <rzimmerman@talusanalytics.com>
2021-08-26 00:24:31 -0600

    Simple filter section placeholder component for thinking about architecture

---
+++ src/components/explorepage/FilterBar/FilterBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FilterSection = styled.section``
+
+const FilterBar: React.FC = () => {
+  return (
+    <FilterSection>
+      <h2>Filters go here</h2>
+    </FilterSection>
+  )
+}
+
+export default FilterBar
