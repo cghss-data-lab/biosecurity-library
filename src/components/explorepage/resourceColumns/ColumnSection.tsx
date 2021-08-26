247d47d1bd1c66b3cd4dcb4de2ed72aaf5b7fa5e
Ryan <rzimmerman@talusanalytics.com>
2021-08-26 00:10:24 -0600

    Adding a column section component in charge of rendering the columns
    This query may be unnecessary because the groups are already known, so
    if the filtering applies only to the entries within each group, the
    groups will still be known after filtering.
    
    So yeah, this query should come out.

---
+++ src/components/explorepage/resourceColumns/ColumnSection.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import Column from './Column'
+
+import { ResourceGroup } from '../../../pages/explore'
+
+const ColumnsContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+`
+
+interface QueryResult {
+  resourceTypes: {
+    distinct: string[]
+  }
+}
+
+const ColumnSection: React.FC<{ resources: ResourceGroup[] }> = ({
+  resources,
+}) => {
+  // the columns get their own query so that we always know
+  // all the columns even if some of the columns are filtered
+  // down to no entries
+
+  // depending on how the filters work, this should be unnecessary...
+  const {
+    resourceTypes: { distinct: columns },
+  }: QueryResult = useStaticQuery(graphql`
+    query resourceTypeQuery {
+      resourceTypes: allAirtable(
+        filter: {
+          table: { eq: "Resource Library" }
+          data: { Publish_INTERNAL: { eq: true } }
+        }
+      ) {
+        distinct(field: data___Resource_Type)
+      }
+    }
+  `)
+
+  console.log(resources)
+
+  return (
+    <ColumnsContainer>
+      {columns.map(name => (
+        <Column
+          key={name}
+          name={name}
+          resources={resources.find(
+            (group: ResourceGroup) => group.fieldValue === name
+          )}
+        />
+      ))}
+    </ColumnsContainer>
+  )
+}
+
+export default ColumnSection
