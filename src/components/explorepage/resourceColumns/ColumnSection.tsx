dad361977c8f6ef6053831305a03f19d8f1574e9
Ryan <rzimmerman@talusanalytics.com>
2021-08-26 01:06:33 -0600

    Cleaning up ColumnSection now that filters work properly and persist groups
    This will get complicated again later (animation and resizing...) so
    it's really important to keep this as simple as possible right now!

--- src/components/explorepage/resourceColumns/ColumnSection.tsx
+++ src/components/explorepage/resourceColumns/ColumnSection.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { useStaticQuery, graphql } from 'gatsby'
 
 import Column from './Column'
 
@@ -11,48 +10,18 @@ const ColumnsContainer = styled.section`
   justify-content: space-between;
 `
 
-// interface QueryResult {
-//   resourceTypes: {
-//     distinct: string[]
-//   }
-// }
-
 const ColumnSection: React.FC<{ resources: ResourceGroup[] }> = ({
   resources,
-}) => {
-  // the columns get their own query so that we always know
-  // all the columns even if some of the columns are filtered
-  // down to no entries
-
-  // depending on how the filters work, this should be unnecessary...
-  // const {
-  //   resourceTypes: { distinct: columns },
-  // }: QueryResult = useStaticQuery(graphql`
-  //   query resourceTypeQuery {
-  //     resourceTypes: allAirtable(
-  //       filter: {
-  //         table: { eq: "Resource Library" }
-  //         data: { Publish_INTERNAL: { eq: true } }
-  //       }
-  //     ) {
-  //       distinct(field: data___Resource_Type)
-  //     }
-  //   }
-  // `)
-
-  console.log(resources)
-
-  return (
-    <ColumnsContainer>
-      {resources.map(group => (
-        <Column
-          key={group.fieldValue}
-          name={group.fieldValue}
-          resources={group}
-        />
-      ))}
-    </ColumnsContainer>
-  )
-}
+}) => (
+  <ColumnsContainer>
+    {resources.map(group => (
+      <Column
+        key={group.fieldValue}
+        name={group.fieldValue}
+        resources={group}
+      />
+    ))}
+  </ColumnsContainer>
+)
 
 export default ColumnSection
