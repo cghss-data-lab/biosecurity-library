30fa42cd2f9a0370d3ccff700ac3e63c834f31ac
Ryan <rzimmerman@talusanalytics.com>
2021-08-30 00:03:48 -0600

    Adding styling to resource preview component

--- src/components/explorepage/resourceColumns/Resource.tsx
+++ src/components/explorepage/resourceColumns/Resource.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 import { Link } from 'gatsby'
 
 import { Resource } from '../../../pages/explore'
 import { urlString } from '../../../airtable-cms/utilities'
+import AirtableCMSIcon from '../../../airtable-cms/AirtableCMSIcon'
+
+const ResourceContainer = styled.section`
+  background: ${({ theme }) => theme.colorVeryLightGray};
+  padding: 20px;
+`
+const Title = styled.div`
+  font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  color: ${({ theme }) => theme.colorBlack};
+  font-weight: bold;
+  font-size: 20px;
+  margin-bottom: 8px;
+`
+
+const SubTitle = styled.div`
+  font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  color: ${({ theme }) => theme.colorVeryDarkGray};
+  font-weight: 500;
+  font-size: 16px;
+`
+
+const IconContainer = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colorMedGray};
+  padding-top: 10px;
+  padding-bottom: 15px;
+  display: flex;
+  gap: 8px;
+`
 
 const ResourcePreview: React.FC<Resource> = ({ data }) => {
+  const theme: any = useTheme()
   return (
-    <p key={data.Short_Name} style={{ background: 'white' }}>
+    <ResourceContainer key={data.Short_Name}>
       <Link
         to={
           '/resource/' +
@@ -15,9 +44,22 @@ const ResourcePreview: React.FC<Resource> = ({ data }) => {
           urlString(data.Short_Name)
         }
       >
-        {data.Resource_Name}
+        <Title>{data.Resource_Name}</Title>
+        <SubTitle>{data.Authoring_Organization}</SubTitle>
+        <IconContainer>
+          <AirtableCMSIcon
+            name="Lab research"
+            color={theme.colorDarkest}
+            style={{ height: 25 }}
+          />
+          <AirtableCMSIcon
+            name="Risk assessment"
+            color={theme.colorDarkest}
+            style={{ height: 25 }}
+          />
+        </IconContainer>
       </Link>
-    </p>
+    </ResourceContainer>
   )
 }
 
