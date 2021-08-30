641f53564d131932fbcd830f0f03bf115cf1f05c
Ryan <rzimmerman@talusanalytics.com>
2021-08-30 13:11:28 -0600

    Adding proper padding and margins inside the detail page tabs

--- src/components/detailpage/TabSection/TabContentContainer.tsx
+++ src/components/detailpage/TabSection/TabContentContainer.tsx
@@ -2,9 +2,18 @@ import styled from 'styled-components'
 
 const TabContentContainer = styled.section`
   grid-area: content;
-  padding: 15px;
+  padding: 0 50px;
   border-top: 1px solid ${({ theme }) => theme.colorMedGray};
   border-left: 1px solid ${({ theme }) => theme.colorMedGray};
+
+  > h5 {
+    margin-top: 30px;
+    margin-bottom: 5px;
+  }
+
+  > p {
+    margin: 0;
+  }
 `
 
 export default TabContentContainer
