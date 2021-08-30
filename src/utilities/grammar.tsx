99925d354c39d5f48903fed0d462e0902ba0b01f
Ryan <rzimmerman@talusanalytics.com>
2021-08-30 11:16:54 -0600

    Adding file for grammar helper functions, commaSeparatedList to start

---
+++ src/utilities/grammar.tsx
@@ -0,0 +1,7 @@
+export const commaSeparatedList = (strings: string[]) => {
+  if (strings.length === 1) return strings[0]
+  const list = strings.slice(0, -1).join(', ')
+  const last = strings[strings.length - 1]
+  const oxfordComma = strings.length > 2 ? ',' : ''
+  return `${list}${oxfordComma} and ${last}`
+}
