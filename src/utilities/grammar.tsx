cc28821cfaa25d2bbaede907efb1305f5312c7c1
Ryan <rzimmerman@talusanalytics.com>
2021-08-30 13:10:18 -0600

    Adding a check if the string array is defined

--- src/utilities/grammar.tsx
+++ src/utilities/grammar.tsx
@@ -1,4 +1,5 @@
 export const commaSeparatedList = (strings: string[]) => {
+  if (!strings) return undefined
   if (strings.length === 1) return strings[0]
   const list = strings.slice(0, -1).join(', ')
   const last = strings[strings.length - 1]
