1d3263b5203b0892cb58a72f0d793def48d95c3b
Ryan <rzimmerman@talusanalytics.com>
2021-08-31 13:07:35 -0600

    Adding the topic icons in the header of the detail page

--- src/components/detailpage/Header/Header.tsx
+++ src/components/detailpage/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
+import AirtableCMSIcon from '../../../airtable-cms/AirtableCMSIcon'
 
 import { PageContext } from '../../../templates/detail'
 
@@ -7,19 +8,37 @@ const HeaderContainer = styled.header`
   grid-area: header;
   padding-left: 50px;
 `
+const IconContainer = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  gap: 20px;
+`
 
-const Header: React.FC<PageContext> = ({ data }) => (
-  <HeaderContainer>
-    <h2>{data.Resource_Name}</h2>
-    <h6>[{data.Short_Name.trim()}]</h6>
-    <h4>
-      by{' '}
-      <strong>
-        <a href="#">{data.Authoring_Organization}</a>
-      </strong>
-    </h4>
-    <p>{data.Short_Description}</p>
-  </HeaderContainer>
-)
+const Header: React.FC<PageContext> = ({ data }) => {
+  const theme: any = useTheme()
+  return (
+    <HeaderContainer>
+      <h2>{data.Resource_Name}</h2>
+      <h6>[{data.Short_Name.trim()}]</h6>
+      <h4>
+        by{' '}
+        <strong>
+          <a href="#">{data.Authoring_Organization}</a>
+        </strong>
+      </h4>
+      <p>{data.Short_Description}</p>
+      <IconContainer>
+        {JSON.parse(data.Topic_Area_Icons).map(({ name }: { name: string }) => (
+          <AirtableCMSIcon
+            key={name}
+            name={name}
+            color={theme.colorOrange}
+            style={{ width: 30 }}
+          />
+        ))}
+      </IconContainer>
+    </HeaderContainer>
+  )
+}
 
 export default Header
