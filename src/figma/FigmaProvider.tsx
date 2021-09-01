bd84c0a09a91e532650d0f9762dcab811ff0cda4
Ryan <rzimmerman@talusanalytics.com>
2021-09-01 17:09:54 -0600

    Eslint disable; because this really is the simplest way to do this at the moment

--- src/figma/FigmaProvider.tsx
+++ src/figma/FigmaProvider.tsx
@@ -6,6 +6,7 @@ import * as styles from './semanticStyles.module.scss'
 import './fonts.css'
 
 // inline webpack require because it's easier that way in gatsby
+// eslint-disable-next-line import/no-webpack-loader-syntax
 const figmaStyles = require('sass-extract-loader?{"plugins":["sass-extract-js"]}!./styles.scss')
 
 const FigmaProvider: React.FC = ({ children }) => (
