3cb97af60e85100a62463d7392a2382bdbf272d9
Ryan <rzimmerman@talusanalytics.com>
2021-09-08 10:58:10 -0600

    refactoring filterbar to use the new state structure
    FilterBar becomes stateless, props are simplified, and new types
    written for the filter components

--- src/components/explorepage/FilterBar/FilterBar.tsx
+++ src/components/explorepage/FilterBar/FilterBar.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css' // optional
 
-import { Definition, Filter } from '../../../pages/explore'
+import { Definition, ExploreState } from '../../../pages/explore'
 import AuthoringOrganization from './filters/AuthoringOrganization'
 import KeyTopic from './filters/KeyTopic'
 import UserRollup from './filters/RecommendedUsership'
@@ -16,12 +16,10 @@ const FilterSection = styled.section`
   gap: 15px;
   margin-top: 50px;
 `
-
 const DefinitionsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `
-
 const DefinitionButton = styled.button`
   border: none;
   background: none;
@@ -32,11 +30,8 @@ const DefinitionButton = styled.button`
   background: rgba(0, 0, 0, 0.025);
 `
 
-// This should be inhereted by each of the filters
+// This interface should be used by each of the filters
 export interface FilterProps {
-  // filters: Filter[]
-  // setFilters: React.Dispatch<React.SetStateAction<Filter[]>>
-  // setExpandDefinitions: React.Dispatch<React.SetStateAction<string>>
   exploreState: ExploreState
   setExploreState: React.Dispatch<React.SetStateAction<ExploreState>>
 }
@@ -48,43 +43,31 @@ export interface FilterOptions {
 
 interface FilterBarProps extends FilterProps {
   definitions: Definition[]
-  expandDefinitions: string
 }
 
 const FilterBar: React.FC<FilterBarProps> = ({
-  filters,
-  setFilters,
   definitions,
-  expandDefinitions,
-  setExpandDefinitions,
-}) => {
-  // const [expandTopic, setExpandTopic] =
-  console.log(expandDefinitions)
-  console.log(
-    definitions.filter(def => def.data.Column[0] === expandDefinitions)
-  )
-  return (
-    <>
-      <DefinitionsContainer>
-        {expandDefinitions !== '' &&
-          definitions
-            .filter(def => def.data.Column[0] === expandDefinitions)
-            .map(def => (
-              <Tippy key={def.data.Glossary_Name} content={def.data.Definition}>
-                <DefinitionButton>{def.data.Glossary_Name}</DefinitionButton>
-              </Tippy>
-            ))}
-      </DefinitionsContainer>
-      <FilterSection>
-        <KeyTopic {...{ filters, setFilters, setExpandDefinitions }} />
-        <TargetUserRole {...{ filters, setFilters, setExpandDefinitions }} />
-        <UserRollup {...{ filters, setFilters, setExpandDefinitions }} />
-        <AuthoringOrganization
-          {...{ filters, setFilters, setExpandDefinitions }}
-        />
-      </FilterSection>
-    </>
-  )
-}
+  exploreState,
+  setExploreState,
+}) => (
+  <>
+    <DefinitionsContainer>
+      {exploreState.defs !== '' &&
+        definitions
+          .filter(def => def.data.Column[0] === exploreState.defs)
+          .map(def => (
+            <Tippy key={def.data.Glossary_Name} content={def.data.Definition}>
+              <DefinitionButton>{def.data.Glossary_Name}</DefinitionButton>
+            </Tippy>
+          ))}
+    </DefinitionsContainer>
+    <FilterSection>
+      <KeyTopic {...{ exploreState, setExploreState }} />
+      <TargetUserRole {...{ exploreState, setExploreState }} />
+      <UserRollup {...{ exploreState, setExploreState }} />
+      <AuthoringOrganization {...{ exploreState, setExploreState }} />
+    </FilterSection>
+  </>
+)
 
 export default FilterBar
