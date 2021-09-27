4f53110a346463315c8eb1b63c643176ea6875db
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2021-09-27 15:05:42 +0000

    Renamin airtable hooks directory

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import ImageHeader from '../components/homepage/ImageHeader'
 import NavBar from '../components/layout/NavBar/NavBar'
 import Main from '../components/layout/Main'
 import IntroSection from '../components/homepage/IntroSection'
-import useHomePageData from '../cmsQueryHooks/useHomePageData'
+import useHomePageData from '../airtableQueryHooks/useHomePageData'
 
 const IndexPage: React.FC<PageProps> = () => {
   const { homePageText, resourceSearchData, homePageResources } =
