b23b7a393d5eee52649ee289986bac2c87c91d52
Ryan <rzimmerman@talusanalytics.com>
2021-10-21 15:30:06 -0600

    Simplifying type declaration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { PageProps } from 'gatsby'
 
 import FigmaProvider from '../figma/FigmaProvider'
 
@@ -9,7 +8,7 @@ import Main from '../components/layout/Main'
 import IntroSection from '../components/homepage/IntroSection'
 import Carousel from '../components/homepage/Carousel/Carousel'
 
-const IndexPage: React.FC<PageProps> = () => (
+const IndexPage = (): JSX.Element => (
   <FigmaProvider>
     <NavBar />
     <ImageHeader />
