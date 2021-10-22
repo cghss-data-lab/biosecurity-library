fd2266382aabd7bd201ff5cb2fd733277291d1e5
Ryan <rzimmerman@talusanalytics.com>
2021-10-22 13:01:33 -0600

    Basic structure of carousel with switcher and display

--- src/components/ui/Carousel/Carousel.tsx
+++ src/components/ui/Carousel/Carousel.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
-// import styled from 'styled-components'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import CarouselDots from './CarouselDots'
 
-const Carousel = ({ children }: { children: React.ReactNode }): JSX.Element => {
-  return <>{children}</>
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+`
+
+interface CarouselProps {
+  children: React.ReactNode
+  className?: string
+}
+
+const Carousel = ({ children, className }: CarouselProps): JSX.Element => {
+  const pages = React.Children.toArray(children) as React.ReactElement[]
+
+  console.log(pages)
+
+  const [active, setActive] = useState(0)
+
+  return (
+    <Section className={className}>
+      <CarouselDots {...{ pages, active, setActive }} />
+      {pages[active]}
+    </Section>
+  )
 }
 
 export default Carousel
