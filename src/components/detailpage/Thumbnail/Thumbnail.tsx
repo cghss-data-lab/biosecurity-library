76a76ed548db4a7d762205679a22cc638b815c78
Ryan <rzimmerman@talusanalytics.com>
2021-10-22 15:24:37 -0600

    Give thumbnail component its own types

--- src/components/detailpage/Thumbnail/Thumbnail.tsx
+++ src/components/detailpage/Thumbnail/Thumbnail.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { PageContext } from '../../../templates/Detail'
+import { GatsbyImage, getImage, ImageDataLike } from 'gatsby-plugin-image'
 
 const ThumbnailContainer = styled.div`
   grid-area: thumbnail;
   padding: 30px 15px 15px 0;
 `
 
-const Thumbnail: React.FC<PageContext> = ({ data }) => {
+interface ThumbnailProps {
+  data: {
+    Thumbnail_INTERNAL: {
+      localFiles: ImageDataLike[]
+    }
+    Short_name: string
+    Files_INTERNAL?: {
+      localFiles: {
+        publicURL: string
+        name: string
+      }[]
+    }
+  }
+}
+
+const Thumbnail = ({ data }: ThumbnailProps): JSX.Element => {
   const thumbnail = getImage(data.Thumbnail_INTERNAL?.localFiles[0])
   if (thumbnail) {
     return (
