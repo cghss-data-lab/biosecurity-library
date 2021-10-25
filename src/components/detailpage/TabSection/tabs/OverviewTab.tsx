38cf6ba2d7db5e5bf8d5daef26024be02c24768f
mvanmaele <mvanmaele@talusanalytics.com>
2021-10-25 10:49:46 -0600

    Add window type check to ResourceMap render

--- src/components/detailpage/TabSection/tabs/OverviewTab.tsx
+++ src/components/detailpage/TabSection/tabs/OverviewTab.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import * as network from '@mvanmaele/mvanmaele-test.viz.network'
 
 import TabContentContainer from '../TabContentContainer'
 import { PageContext } from '../../../../templates/Detail'
 import { commaSeparatedList } from '../../../../utilities/grammar'
 import ResourceMap from '../../ResourceMap/ResourceMap'
-import { InfoTip } from '../../../ui/InfoTip'
 
 const OverviewTab: React.FC<PageContext> = ({ data }) => {
-  const graphData: network.AppGraphData =
-    data.resourceMapData !== undefined
-      ? data.resourceMapData
-      : network.emptyGraphData
   return (
     <TabContentContainer>
       <h5>SUMMARY</h5>
@@ -20,12 +14,11 @@ const OverviewTab: React.FC<PageContext> = ({ data }) => {
       <p>{data.Resource_type}</p>
       <h5>CATEGORIES</h5>
       <p>{commaSeparatedList(data.Key_topic_area)}</p>
-      {graphData && graphData.links.length > 0 && (
+      {typeof window !== 'undefined' && data.resourceMapData !== undefined && (
         <>
           <h5>RESOURCE MAP</h5>
-          <InfoTip />
           <p>
-            <ResourceMap selectedNode={data} graphData={graphData} />
+            <ResourceMap selectedNode={data} graphData={data.resourceMapData} />
           </p>
         </>
       )}
