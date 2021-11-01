8f0cb900946cf40828c0a0c2ee852c09c0704d19
Ryan <rzimmerman@talusanalytics.com>
2021-11-01 11:27:40 -0600

    Refactoring index.tsx and IntroSection.tsx
    This makes index.tsx a better "roadmap" of what components do what and
    keeps IntroSection.tsx as a simple text display component

--- src/components/homepage/IntroSection.tsx
+++ src/components/homepage/IntroSection.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-// import AirtableCMSIcon from '../../airtable-cms/AirtableCMSIcon'
+
 import AirtableCMSText from '../../airtable-cms/AirtableCMSText'
 import useHomePageData from '../../airtableQueryHooks/useHomePageData'
 
-import BarChart from './barChart/BarChart'
-import CarouselSection from './CarouselSection/CarouselSection'
-import ResourceSearch from './ResourceSearch'
-
-// import { useTheme } from 'styled-components'
-
 const Section = styled.section`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  max-width: 900px;
-  margin: 0 auto;
-
   h1 {
     text-align: center;
     margin-bottom: 0px;
@@ -41,9 +29,6 @@ const IntroSection = (): JSX.Element => {
       <h3>
         <AirtableCMSText name={'First Paragraph'} data={homePageText} />
       </h3>
-      <ResourceSearch />
-      <CarouselSection />
-      <BarChart />
     </Section>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,18 @@ import NavBar from '../components/layout/NavBar/NavBar'
 import Main from '../components/layout/Main'
 import IntroSection from '../components/homepage/IntroSection'
 import useHomePageData from '../airtableQueryHooks/useHomePageData'
+import styled from 'styled-components'
+import ResourceSearch from '../components/homepage/ResourceSearch'
+import CarouselSection from '../components/homepage/CarouselSection/CarouselSection'
+import BarChart from '../components/homepage/barChart/BarChart'
+
+const HomepageMain = styled(Main)`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  max-width: 900px;
+  margin: 0 auto;
+`
 
 const IndexPage = (): JSX.Element => {
   const { homePageText } = useHomePageData()
@@ -14,9 +26,12 @@ const IndexPage = (): JSX.Element => {
     <FigmaProvider>
       <NavBar />
       <ImageHeader cmsData={homePageText} />
-      <Main>
+      <HomepageMain>
         <IntroSection />
-      </Main>
+        <ResourceSearch />
+        <CarouselSection />
+        <BarChart />
+      </HomepageMain>
     </FigmaProvider>
   )
 }
