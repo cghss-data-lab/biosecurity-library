b6f7e1060e8c2ded45453650056d7aeb6ab18bb3
Ryan <rzimmerman@talusanalytics.com>
2021-11-03 17:38:27 -0600

    Adding mouse hover handling to flip the colors of the bar and icon

--- src/components/homepage/barChart/Bar.tsx
+++ src/components/homepage/barChart/Bar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTheme } from 'styled-components'
 
 import AirtableCMSPlotIcon from '../../../airtable-cms/AirtableCMSPlotIcon'
@@ -17,20 +17,26 @@ interface BarProps {
 const Bar = ({ index, bar, dim }: BarProps): JSX.Element => {
   const theme: any = useTheme()
 
+  const [hover, setHover] = useState(false)
+
   return (
     <>
       <rect
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
         key={index}
         x={index * (dim.barWidth + dim.barGap)}
         y={(dim.maxBar - bar.totalCount) * dim.scale}
         width={dim.barWidth}
         height={dim.scale * bar.totalCount}
-        fill={theme.colorDarkest}
+        fill={hover ? theme.colorGolden : theme.colorDarkest}
+        stroke={theme.colorDarkest}
       />
       <AirtableCMSPlotIcon
         noEmitError
+        style={{ pointerEvents: 'none' }}
         name={bar.fieldValue}
-        color={theme.colorGolden}
+        color={hover ? theme.colorDarkest : theme.colorGolden}
         x={index * (dim.barWidth + dim.barGap) + 0.5 * dim.barWidth}
         y={
           (dim.maxBar - bar.totalCount) * dim.scale +
