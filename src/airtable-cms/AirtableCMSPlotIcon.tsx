13ad3b825838b74a47b307c1ce2e7a794f6557d1
Ryan <rzimmerman@talusanalytics.com>
2021-11-03 14:54:45 -0600

    New AirtableCMS Component which places an icon centered on a point in an SVG

---
+++ src/airtable-cms/AirtableCMSPlotIcon.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import AirtableCMSIcon from '../../../airtable-cms/AirtableCMSIcon'
+
+const IconHolder = styled.foreignObject`
+  overflow: visible;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+interface SVGIconProps {
+  /** X position of the center of the icon */
+  x: number
+  /** Y position of the center of the icon */
+  y: number
+  /** Icon container height */
+  height: number
+  /** Icon container width */
+  width: number
+  /** Airtable Icon Name (will get passed to AirtableCMSIcon) */
+  name: string
+  /** Icon Color */
+  color: string
+  /** Icon hover color */
+  hoverColor?: string
+  /** Suppress AirtableCMSIcon missing icon error */
+  noEmitError?: boolean
+}
+
+const AirtableCMSPlotIcon = ({
+  x,
+  y,
+  height,
+  width,
+  name,
+  color,
+  hoverColor,
+  noEmitError,
+}: SVGIconProps): JSX.Element => {
+  return (
+    <IconHolder x={x - width / 2} y={y - height / 2} {...{ width, height }}>
+      <AirtableCMSIcon
+        style={{ width, height }}
+        {...{ name, color, hoverColor, noEmitError }}
+      />
+    </IconHolder>
+  )
+}
+
+export default AirtableCMSPlotIcon
