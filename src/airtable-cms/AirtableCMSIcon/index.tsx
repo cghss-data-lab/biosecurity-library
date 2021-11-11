18eed1c88647d6f192f037df6f36fda054ede8d8
Ryan <rzimmerman@talusanalytics.com>
2021-11-11 15:08:51 -0700

    adding index.tsx file

---
+++ src/airtable-cms/AirtableCMSIcon/index.tsx
@@ -0,0 +1,14 @@
+import AirtableCMSIcon from './AirtableCMSIcon'
+import replaceFill from './replaceFill'
+import useAllAirtableIcons from './useAllAirtableIcons'
+import useAirtableIcon from './useAirtableIcon'
+
+export {
+  // hooks
+  useAllAirtableIcons,
+  useAirtableIcon,
+  // utility function
+  replaceFill,
+}
+
+export default AirtableCMSIcon
