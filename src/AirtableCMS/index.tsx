1293e63b1f6a8d1e50317512be3ce3ac826bd311
Ryan <rzimmerman@talusanalytics.com>
2021-11-16 17:25:23 -0700

    Adding CMSRichText component to Airtable CMS and using it for long descriptions

--- src/AirtableCMS/index.tsx
+++ src/AirtableCMS/index.tsx
@@ -10,6 +10,10 @@ import CMSImage, {
   getCMSImage,
   CMSImageProps,
 } from '@talus-analytics/library.airtable.cms-image'
+import CMSRichText, {
+  RenderCMSRichText,
+  parseCMSRichText,
+} from '@talus-analytics/library.airtable.cms-rich-text'
 import type { AirtableCMSData } from '@talus-analytics/library.airtable.cms-types'
 
 export type { AirtableCMSData, CMSTextProps, CMSIconProps, CMSImageProps }
@@ -18,12 +22,17 @@ const CMS = {
   // components
   Image: CMSImage,
   Text: CMSText,
+  RichText: CMSRichText,
   Icon: CMSIcon,
+  // non-AirtableCMSData components
+  RenderRichText: RenderCMSRichText,
   // getters
   getImage: getCMSImage,
   getText: getCMSText,
   // hooks
   useIcon: useCMSIcon,
+  // utiliries
+  parseRichText: parseCMSRichText,
 }
 
 export default CMS
