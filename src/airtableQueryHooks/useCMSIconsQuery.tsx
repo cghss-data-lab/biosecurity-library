5e8260f753d5841addc3fb408db545800c9b7fe7
Ryan <rzimmerman@talusanalytics.com>
2021-11-18 13:29:24 -0700

    Refactoring CMSIcons component to use context and and an external query so it won't break gatsby builds

--- src/airtableQueryHooks/useCMSIconsQuery.tsx
+++ src/airtableQueryHooks/useCMSIconsQuery.tsx
@@ -1,30 +1,12 @@
 import { useStaticQuery, graphql } from 'gatsby'
+import { IconsQuery } from '@talus-analytics/library.airtable.cms-icon'
 
 // This query and interface lives here because it assumes the
 // CMS airtable base has this table already, because the table
 // will be part of the template.
-export interface IconsQuery {
-  iconsQuery: {
-    nodes: {
-      data: {
-        Name: string
-        Text: string
-        SVG: {
-          localFiles: {
-            childSvg: {
-              svgString: string
-            }
-          }[]
-        }
-      }
-    }[]
-  }
-}
 
 const useCMSIconsQuery = () => {
-  const {
-    iconsQuery: { nodes },
-  } = useStaticQuery<IconsQuery>(graphql`
+  const iconsQuery = useStaticQuery<IconsQuery>(graphql`
     query iconsQuery {
       iconsQuery: allAirtable(filter: { table: { eq: "Icons" } }) {
         nodes {
@@ -44,7 +26,7 @@ const useCMSIconsQuery = () => {
     }
   `)
 
-  return nodes
+  return iconsQuery
 }
 
 export default useCMSIconsQuery
