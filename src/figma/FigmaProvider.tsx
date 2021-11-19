1359961d4d98bfcf8e00c538d484e1b707b22ac2
Ryan <rzimmerman@talusanalytics.com>
2021-11-19 10:58:11 -0700

    Dropping cache for testing

--- src/figma/FigmaProvider.tsx
+++ src/figma/FigmaProvider.tsx
@@ -5,14 +5,15 @@ import { semanticStyles } from './semanticStyles.module.scss'
 
 import './fonts.css'
 
-import CMS, { useIconsQuery } from '@talus-analytics/library.airtable-cms'
+import CMS from '@talus-analytics/library.airtable-cms'
+import useCMSIconsQuery from '../airtableQueryHooks/useCMSIconsQuery'
 
 // inline webpack require because it's easier that way in gatsby
 // eslint-disable-next-line import/no-webpack-loader-syntax
 const figmaStyles = require('sass-extract-loader?{"plugins":["sass-extract-js"]}!./styles.scss')
 
 const FigmaProvider: React.FC = ({ children }) => {
-  const icons = useIconsQuery()
+  const icons = useCMSIconsQuery()
 
   return (
     <ThemeProvider theme={figmaStyles}>
