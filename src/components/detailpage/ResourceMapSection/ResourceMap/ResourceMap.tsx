25f8f3f25c17e58cb518a74cf322db1306bdc5a9
mvanmaele <mvanmaele@talusanalytics.com>
2021-11-24 12:10:40 -0500

    Upgrade network bit.dev component

--- src/components/detailpage/ResourceMapSection/ResourceMap/ResourceMap.tsx
+++ src/components/detailpage/ResourceMapSection/ResourceMap/ResourceMap.tsx
@@ -16,8 +16,7 @@ import {
   useAllCMSIcons,
   replaceFill,
 } from '@talus-analytics/library.airtable.cms-icon'
-import * as network from '../../../../network'
-// import * as network from '@talus-analytics/viz.charts.network'
+import * as network from '@talus-analytics/viz.charts.network'
 import {
   SettingsContext,
   defaultSettings,
