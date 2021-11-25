7890776158f9ef29686091cc169736b4a340df99
Ryan <rzimmerman@talusanalytics.com>
2021-11-24 17:04:06 -0700

    Adding simple accessibility banner component

---
+++ src/components/ui/AccessibilityBanner/AccessibilityBanner.tsx
@@ -0,0 +1,44 @@
+import CMS from '@talus-analytics/library.airtable-cms'
+import React from 'react'
+import styled, { useTheme } from 'styled-components'
+
+const Container = styled.section`
+  display: flex;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colorYellow};
+  background-color: ${({ theme }) => theme.colorVeryLightYellow};
+  border-radius: 5px;
+  padding: 15px;
+  flex-grow: 0;
+  width: fit-content;
+  margin: 0 auto;
+
+  p {
+    font-family: Open Sans, Arial, Helvetica, sans-serif !important;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px !important;
+    line-height: 22px;
+  }
+`
+
+const AccessibilityBanner = ({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element => {
+  const theme: any = useTheme()
+
+  return (
+    <Container>
+      <CMS.Icon
+        name="Notice"
+        color={theme.colorGolden}
+        style={{ position: 'relative', top: -1, marginRight: 2 }}
+      />
+      {children}
+    </Container>
+  )
+}
+
+export default AccessibilityBanner
