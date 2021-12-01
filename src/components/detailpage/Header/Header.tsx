0738a1b286272fcec68f976082d3eeaabbd0c3fa
Ryan <rzimmerman@talusanalytics.com>
2021-11-30 18:40:02 -0700

    Adding resource type icon tag

--- src/components/detailpage/Header/Header.tsx
+++ src/components/detailpage/Header/Header.tsx
@@ -4,6 +4,7 @@ import styled, { useTheme } from 'styled-components'
 import CMS from '@talus-analytics/library.airtable-cms'
 
 import { PageContext } from '../../../templates/Detail'
+import IconTag from 'components/ui/IconTag/IconTag'
 
 const HeaderContainer = styled.header`
   grid-area: header;
@@ -23,6 +24,7 @@ const Header: React.FC<PageContext> = ({ data }) => {
       <h2>{data.Resource_name}</h2>
       <h6>[{data.Short_name.trim()}]</h6>
       <h4>{data.Authoring_organization[0].data.Name}</h4>
+      <IconTag dark name={data.Resource_type} style={{ marginTop: 35 }} />
       <p>{data.Short_description}</p>
       <IconContainer>
         {data.Key_topic_area.map(name => (
