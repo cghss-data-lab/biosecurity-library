20f133f6f6d69a6af455c26cc64017c5a5f9c84e
Ryan <rzimmerman@talusanalytics.com>
2021-12-01 10:27:31 -0700

    Add info icon and popup text

--- src/components/explorepage/resourceColumns/ColumnSection.tsx
+++ src/components/explorepage/resourceColumns/ColumnSection.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 
 import Column from './Column'
 
-import { ExploreState } from '../../../pages/explore'
-import { ResourceGroup } from '../../../airtableQueryHooks/useExplorePageData'
+import { ExploreState } from 'pages/explore'
+import useExplorePageData, {
+  ResourceGroup,
+} from 'airtableQueryHooks/useExplorePageData'
+import Tippy from '@tippyjs/react'
+import CMS from 'AirtableCMS'
 
 const ColumnsContainer = styled.section`
   display: flex;
@@ -18,6 +22,14 @@ const ColumnsContainer = styled.section`
 `
 const ResourceCount = styled.div`
   margin-top: 15px;
+`
+const InfoIcon = styled(CMS.Icon)`
+  height: 1.2em;
+  width: 1.2em;
+  display: inline-block;
+  position: relative;
+  top: 0.2em;
+  margin-left: 0.2em;
 `
 
 interface ColumnSectionProps {
@@ -48,10 +60,16 @@ const ColumnSection: React.FC<ColumnSectionProps> = ({
     0
   )
 
+  const { explorePageText } = useExplorePageData()
+  const theme: any = useTheme()
+
   return (
     <>
       <ResourceCount>
         {resourceCount} {resourceCount === 1 ? 'Result' : 'Results'}
+        <Tippy content={<CMS.Text name="Sort info" data={explorePageText} />}>
+          <InfoIcon name="Info alternate" color={theme.colorBlack} />
+        </Tippy>
       </ResourceCount>
       <ColumnsContainer>
         {displayResources.map(group => (
