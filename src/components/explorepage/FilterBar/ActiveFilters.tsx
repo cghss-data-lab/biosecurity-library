95fdb9266632f2df7362bfc3a340d8ec48f29b99
Ryan <rzimmerman@talusanalytics.com>
2021-12-01 15:55:39 -0700

    Adding remove filters button (works) and more filters button (doesn't work)

--- src/components/explorepage/FilterBar/ActiveFilters.tsx
+++ src/components/explorepage/FilterBar/ActiveFilters.tsx
@@ -3,34 +3,62 @@ import styled from 'styled-components'
 import RemoveFilterButton from './RemoveFilterButton'
 
 import { FilterProps } from './FilterBar'
-import { removeFilter } from './filterOperations'
+import { removeAllFilters, removeFilter } from './filterOperations'
 
+const Section = styled.section`
+  display: flex;
+  align-items: flex-start;
+  margin-top: 30px;
+`
 const FilterContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 5px;
-  margin-top: 30px;
+`
+const ClearFilters = styled.button`
+  background: none;
+  border: none;
+  border: 1px solid ${({ theme }) => theme.colorBlack};
+  padding: 0.5em 1em;
+  border-radius: 5px;
+  flex-shrink: 0;
+  box-shadow: 2px 2px 12px 0px rgba(0, 0, 0, 0);
+  transition: 250ms ease;
+  margin-left: auto;
+
+  &:hover {
+    box-shadow: 2px 2px 12px 0px rgba(0, 0, 0, 0.25);
+  }
+`
+const MoreFilters = styled(ClearFilters)`
+  margin-left: 0.5em;
 `
 
 const ActiveFilters: React.FC<FilterProps> = ({
   exploreState,
   setExploreState,
 }) => (
-  <FilterContainer>
-    {exploreState.filters &&
-      Object.entries(exploreState.filters).map(([filterName, filterValues]) =>
-        filterValues.map(value => (
-          <RemoveFilterButton
-            key={value}
-            onClick={() =>
-              removeFilter({ [filterName]: [value] }, setExploreState)
-            }
-          >
-            {filterName.replace(/_/g, ' ')}: {value}
-          </RemoveFilterButton>
-        ))
-      )}
-  </FilterContainer>
+  <Section>
+    <FilterContainer>
+      {exploreState.filters &&
+        Object.entries(exploreState.filters).map(([filterName, filterValues]) =>
+          filterValues.map(value => (
+            <RemoveFilterButton
+              key={value}
+              onClick={() =>
+                removeFilter({ [filterName]: [value] }, setExploreState)
+              }
+            >
+              {filterName.replace(/_/g, ' ')}: {value}
+            </RemoveFilterButton>
+          ))
+        )}
+    </FilterContainer>
+    <ClearFilters onClick={() => removeAllFilters(setExploreState)}>
+      Clear filters
+    </ClearFilters>
+    <MoreFilters>+ More filters</MoreFilters>
+  </Section>
 )
 
 export default ActiveFilters
