69ca2689db5d7858c35ead7c15c21c2a956f6d47
Ryan <rzimmerman@talusanalytics.com>
2021-11-30 18:22:21 -0700

    Add spread div props to IconTag

--- src/components/ui/IconTag/IconTag.tsx
+++ src/components/ui/IconTag/IconTag.tsx
@@ -3,36 +3,38 @@ import React from 'react'
 import styled, { useTheme } from 'styled-components'
 
 const Tag = styled.div<{ dark: boolean }>`
-  padding: 0.25em 1em;
+  padding: 0.3em 1em;
   border: 1px solid ${({ theme }) => theme.colorDarkest};
   border-radius: 2em;
   width: fit-content;
   display: flex;
   align-items: center;
   color: ${({ theme, dark }) => (dark ? theme.colorWhite : theme.colorDarkest)};
-  margin-bottom: 1em;
+  font-size: 16px;
   ${({ theme, dark }) => dark && `background-color: ${theme.colorDarkest}`}
 `
 
-interface IconTagProps {
+interface IconTagProps extends React.ComponentPropsWithoutRef<'div'> {
   name: string
   dark?: boolean
 }
 
-const IconTag = ({ name, dark = false }: IconTagProps): JSX.Element => {
+const IconTag = ({
+  name,
+  dark = false,
+  ...props
+}: IconTagProps): JSX.Element => {
   const theme: any = useTheme()
 
   return (
-    <Tag dark={dark}>
+    <Tag dark={dark} {...props}>
       <CMS.Icon
         name={name}
         color={dark ? theme.colorWhite : theme.colorDarkest}
         style={{
-          height: '1.5em',
-          width: '1.5em',
+          height: '1.4em',
+          width: '1.4em',
           marginRight: '0.375em',
-          position: 'relative',
-          top: -1,
         }}
       />
       {name}
