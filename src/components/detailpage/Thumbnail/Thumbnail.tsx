8ad66a1b20fec53c90c96032ff98bd32247f20a6
Ryan <rzimmerman@talusanalytics.com>
2021-12-09 18:03:17 -0700

    Adding download buttons for files and urls with fallback modes

--- src/components/detailpage/Thumbnail/Thumbnail.tsx
+++ src/components/detailpage/Thumbnail/Thumbnail.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 import { GatsbyImage, getImage, ImageDataLike } from 'gatsby-plugin-image'
 
-import Dropdown from 'components/ui/Dropdown'
+import Dropdown from '@talus-analytics/library.ui.dropdown'
 
 const ThumbnailContainer = styled.div`
   grid-area: thumbnail;
@@ -22,7 +22,7 @@ interface ThumbnailProps {
     }
     Short_name: string
     Other_language_URLs: string
-    Resource_language: string[]
+    URL_for_resource: string
     Files_INTERNAL?: {
       localFiles: {
         publicURL: string
@@ -39,21 +39,56 @@ interface ThumbnailProps {
 }
 
 const DropdownButton = styled.button<{ open: boolean; animDuration: number }>`
+  color: ${({ theme }) => theme.colorBlack} !important;
   background: linear-gradient(
       180deg,
       rgba(255, 255, 255, 0.3) 0%,
       rgba(255, 255, 255, 0) 100%
     ),
     #bdd9f2;
-  /* Lighter */
 
   border: 2px solid #bdd9f2;
   box-sizing: border-box;
   box-shadow: 0px 1.5px 4px rgba(0, 0, 0, 0.25);
   border-radius: 2px;
-  transition: ${({ animDuration }) => animDuration + 'ms ease'};
-  margin-top: 30px;
+  margin-top: 25px;
   padding: 10px 20px;
+  transition: ${({ animDuration }) => animDuration + 'ms ease'};
+
+  ${({ open }) =>
+    open &&
+    `
+    background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.3) 0%,
+      rgba(255, 255, 255, 0) 100%
+    ),
+    #9bc1e2;
+    border: 2px solid #a7c6e1;
+  `}
+`
+
+const DownloadLinkButton = styled.a`
+  display: block;
+  width: fit-content;
+  color: ${({ theme }) => theme.colorBlack} !important;
+  background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.3) 0%,
+      rgba(255, 255, 255, 0) 100%
+    ),
+    #bdd9f2;
+
+  border: 2px solid #bdd9f2;
+  box-sizing: border-box;
+  box-shadow: 0px 1.5px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 2px;
+  margin-top: 25px;
+  padding: 10px 20px;
+
+  /* &:hover {
+    text-decoration: none !important;
+  } */
 `
 
 const DownloadContainer = styled.div`
@@ -65,12 +100,6 @@ const Thumbnail = ({ data }: ThumbnailProps): JSX.Element => {
   const thumbnail = getImage(data.Thumbnail_INTERNAL?.localFiles[0])
 
   // console.log(data.Other_language_files_INTERNAL)
-
-  // const languageURLs: { [key: string]: string } = {}
-  // data.Other_language_URLs.split('\n').forEach(row => {
-  //   const [lang, url] = row.split(': ')
-  //   languageURLs[lang] = url
-  // })
 
   // console.log(languageURLs)
 
@@ -86,7 +115,18 @@ const Thumbnail = ({ data }: ThumbnailProps): JSX.Element => {
     languageFiles[lang] = file.publicURL
   })
 
+  const languageURLs: { [key: string]: string } = {}
+  if (Object.keys(languageFiles).length === 0) {
+    if (data.URL_for_resource) languageURLs['English'] = data.URL_for_resource
+
+    data.Other_language_URLs?.split('\n').forEach(row => {
+      const [lang, url] = row.split(': ')
+      languageURLs[lang] = url
+    })
+  }
+
   console.log(languageFiles)
+  console.log(languageURLs)
 
   if (thumbnail) {
     return (
@@ -100,26 +140,51 @@ const Thumbnail = ({ data }: ThumbnailProps): JSX.Element => {
             }}
           />
         )}
-        {data.Files_INTERNAL && (
-          <div style={{ display: 'flex' }}>
-            <Dropdown
-              hover
-              renderButton={(open, animDuration) => (
-                <DropdownButton {...{ open, animDuration }}>
-                  Download
-                  {/* {Object.keys(languageFiles).length} languages */}
-                </DropdownButton>
-              )}
-            >
-              <DownloadContainer>
-                {Object.entries(languageFiles).map(([lang, url]) => (
-                  <p>
-                    <a href={url}>{lang}</a>
-                  </p>
-                ))}
-              </DownloadContainer>
-            </Dropdown>
-          </div>
+        {Object.keys(languageFiles).length === 1 && (
+          <DownloadLinkButton href={languageFiles.English}>
+            Download
+          </DownloadLinkButton>
+        )}
+        {Object.keys(languageFiles).length > 1 && (
+          <Dropdown
+            hover
+            renderButton={(open, animDuration) => (
+              <DropdownButton {...{ open, animDuration }}>
+                Download
+              </DropdownButton>
+            )}
+          >
+            <DownloadContainer>
+              {Object.entries(languageFiles).map(([lang, url]) => (
+                <p key={url}>
+                  <a href={url}>{lang}</a>
+                </p>
+              ))}
+            </DownloadContainer>
+          </Dropdown>
+        )}
+        {Object.keys(languageURLs).length === 1 && (
+          <DownloadLinkButton href={languageURLs.English}>
+            Access
+          </DownloadLinkButton>
+        )}
+        {Object.keys(languageURLs).length > 1 && (
+          <Dropdown
+            hover
+            renderButton={(open, animDuration) => (
+              <DropdownButton {...{ open, animDuration }}>
+                Access
+              </DropdownButton>
+            )}
+          >
+            <DownloadContainer>
+              {Object.entries(languageURLs).map(([lang, url]) => (
+                <p key={lang}>
+                  <a href={url ?? lang}>{lang}</a>
+                </p>
+              ))}
+            </DownloadContainer>
+          </Dropdown>
         )}
       </ThumbnailContainer>
     )
