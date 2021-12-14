b5b3fb0c81b340a9edc268633a83f706cfada020
Ryan <rzimmerman@talusanalytics.com>
2021-12-14 10:38:14 -0700

    dark mode examples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,20 @@ import ResourceSearch from '../components/homepage/ResourceSearch'
 import CarouselSection from '../components/homepage/CarouselSection/CarouselSection'
 import BarChart from '../components/homepage/barChart/BarChart'
 import Footer from 'components/layout/Footer'
+// import useDarkPreference from 'hooks/useDarkPreference'
+// import styled from 'styled-components'
+
+// const Example = styled.div`
+//   width: 100%;
+//   height: 100vh;
+//   transition: 250ms ease;
+//   display: flex;
+//   align-items: center;
+// `
 
 const IndexPage = (): JSX.Element => {
+  // const userPrefersDark = useDarkPreference()
+
   const { homePageText } = useHomePageData()
   return (
     <FigmaProvider>
@@ -20,6 +32,13 @@ const IndexPage = (): JSX.Element => {
       <ResourceSearch />
       <CarouselSection />
       <BarChart />
+      {/* <Example
+        style={{ backgroundColor: userPrefersDark ? '#2C232E' : '#F5F0F2' }}
+      >
+        <p style={{ color: userPrefersDark ? '#F5F0F2' : '#2C232E' }}>
+          {userPrefersDark ? 'Darkmode!' : 'Lightmode!'}
+        </p>
+      </Example> */}
       <Footer />
     </FigmaProvider>
   )
