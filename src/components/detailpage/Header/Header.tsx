b3486c3036bb82525ddf59405b27e6fe4ddac6e0
Ryan <rzimmerman@talusanalytics.com>
2022-01-13 10:03:45 -0700

    Make header match the carousel card

--- src/components/detailpage/Header/Header.tsx
+++ src/components/detailpage/Header/Header.tsx
@@ -16,8 +16,8 @@ const IconContainer = styled.div`
   gap: 20px;
 `
 const Author = styled.div`
-  font-size: 22px;
-  line-height: 27px;
+  font-size: 26px;
+  line-height: 32px;
   margin-bottom: 20px;
   color: ${({ theme }) => theme.colorDarkGray};
 `
@@ -26,38 +26,34 @@ const ShortName = styled.h6`
   margin-bottom: 20px;
   font-style: normal;
   font-weight: normal;
-  font-size: 16px;
+  font-size: 16px !important;
   line-height: 22px;
+  font-family: 'Open Sans' !important;
+  margin-top: 0px !important;
 `
 const TagHolder = styled.div`
   display: flex;
   margin-top: 35px;
 `
 
-const Header: React.FC<PageContext> = ({ data }) => {
-  const theme: any = useTheme()
-
-  console.log(data)
-
-  return (
-    <HeaderContainer>
-      <h2 style={{ marginTop: 30 }}>{data.Resource_name}</h2>
-      <ShortName>[{data.Short_name.trim()}]</ShortName>
-      <Author>{data.Authoring_organization[0].data.value}</Author>
-      <p>{data.Short_description}</p>
-      <TagHolder>
-        {data.Seminal_resource === 'Yes' && (
-          <IconTag dark name={'Key resource'} style={{ marginRight: '1em' }} />
-        )}
-        <IconTag dark name={data.Resource_type} />
-      </TagHolder>
-      <IconContainer>
-        {data.Key_topic_area.map(name => (
-          <IconTag name={name} />
-        ))}
-      </IconContainer>
-    </HeaderContainer>
-  )
-}
+const Header: React.FC<PageContext> = ({ data }) => (
+  <HeaderContainer>
+    <h2 style={{ marginTop: 30, marginBottom: 0 }}>{data.Resource_name}</h2>
+    <ShortName>[{data.Short_name.trim()}]</ShortName>
+    <Author>{data.Authoring_organization[0].data.value}</Author>
+    <p>{data.Short_description}</p>
+    <TagHolder>
+      {data.Seminal_resource === 'Yes' && (
+        <IconTag dark name={'Key resource'} style={{ marginRight: '1em' }} />
+      )}
+      <IconTag dark name={data.Resource_type} />
+    </TagHolder>
+    <IconContainer>
+      {data.Key_topic_area.map(name => (
+        <IconTag name={name} />
+      ))}
+    </IconContainer>
+  </HeaderContainer>
+)
 
 export default Header
