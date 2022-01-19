92d92919499a723530f694b3403edda2517f29ee
Ryan <rzimmerman@talusanalytics.com>
2022-01-19 09:57:59 -0700

    Change legend words from "citing" to "mentioning" language

--- src/components/detailpage/ResourceMapSection/ResourceMap/Legend/CurvedEdgeEntry.tsx
+++ src/components/detailpage/ResourceMapSection/ResourceMap/Legend/CurvedEdgeEntry.tsx
@@ -10,7 +10,7 @@ const LinkLegendContainer = styled.div`
   position: relative;
   width: 150px;
   height: 100px;
-  margin-left: -20px;
+  margin-left: -7px;
   margin-top: -5px !important;
 `
 
@@ -18,14 +18,14 @@ export const CurvedEdgeEntry = ({ nodeColor }: { nodeColor: string }) => {
   const theme: any = useTheme()
   const legendGraphData: AppGraphData = useMemo(() => {
     return {
-      nodes: [initNode('Cited', 1), initNode('Citing', 2)].map(n => {
+      nodes: [initNode('Mentioned', 1), initNode('Mentioning', 2)].map(n => {
         return {
           ...n,
           _color: nodeColor,
           _labelColor: theme.colorBlack,
           _labelFontWeight: 'normal',
           _labelYOffset: 0,
-          _fontSize: 14.4,
+          _fontSize: 13.4,
         }
       }),
       links: [{ source: 2, target: 1, value: 1 }],
