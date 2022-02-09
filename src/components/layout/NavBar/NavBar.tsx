b7ee3e7e326cd34602def3a3041ec10cbcdbc729
Ryan <rzimmerman@talusanalytics.com>
2022-02-09 15:08:12 -0700

    Nav Bar changes

--- src/components/layout/NavBar/NavBar.tsx
+++ src/components/layout/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import styled, { useTheme } from 'styled-components'
 import NavLink from './NavLink'
 
 import Typeahead from '@talus-analytics/library.ui.typeahead'
@@ -37,6 +37,14 @@ const HomeLink = styled(NavLink)`
   font-size: 24px !important;
   color: white;
   padding: 0;
+  font-weight: 500;
+`
+
+const NavTypeahead = styled(Typeahead)`
+  > input {
+    border-radius: 2px;
+    font-size: 16px;
+  }
 `
 
 const NavBar: React.FC = () => {
@@ -50,12 +58,15 @@ const NavBar: React.FC = () => {
   }))
 
   const [focusInSearch, setFocusInSearch] = useState(false)
+  const theme: any = useTheme()
 
   return (
     <>
       <Nav>
         <Container>
-          <HomeLink to="/">Biosecurity Central</HomeLink>
+          <HomeLink style={{ fontWeight: '500' }} to="/">
+            Biosecurity Central
+          </HomeLink>
           <LinkList>
             <div
               style={{
@@ -66,7 +77,7 @@ const NavBar: React.FC = () => {
               onFocus={() => setFocusInSearch(true)}
               onBlur={() => setTimeout(() => setFocusInSearch(false), 200)}
             >
-              <Typeahead
+              <NavTypeahead
                 style={{
                   margin: 0,
                   minWidth: 400,
@@ -76,7 +87,7 @@ const NavBar: React.FC = () => {
                 iconLeft
                 fontColor={focusInSearch ? 'black' : 'white'}
                 // borderColor="#5B6476"
-                borderColor="#EDA632"
+                borderColor={theme.colorGolden}
                 backgroundColor={focusInSearch ? 'white' : '#082E51'}
                 items={resources}
                 placeholder={`Search`}
@@ -92,7 +103,7 @@ const NavBar: React.FC = () => {
                       })
                     )
                 }}
-                iconSVG={`%3Csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.01 11.255H12.22L11.94 10.985C12.92 9.845 13.51 8.365 13.51 6.755C13.51 3.165 10.6 0.254997 7.01001 0.254997C3.42001 0.254997 0.51001 3.165 0.51001 6.755C0.51001 10.345 3.42001 13.255 7.01001 13.255C8.62001 13.255 10.1 12.665 11.24 11.685L11.51 11.965V12.755L16.51 17.745L18 16.255L13.01 11.255ZM7.01001 11.255C4.52001 11.255 2.51001 9.245 2.51001 6.755C2.51001 4.265 4.52001 2.255 7.01001 2.255C9.50001 2.255 11.51 4.265 11.51 6.755C11.51 9.245 9.50001 11.255 7.01001 11.255Z' fill='%23757C85'/%3E%3C/svg%3E%0A`}
+                iconSVG={`%3Csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.01 11.255H12.22L11.94 10.985C12.92 9.845 13.51 8.365 13.51 6.755C13.51 3.165 10.6 0.254997 7.01001 0.254997C3.42001 0.254997 0.51001 3.165 0.51001 6.755C0.51001 10.345 3.42001 13.255 7.01001 13.255C8.62001 13.255 10.1 12.665 11.24 11.685L11.51 11.965V12.755L16.51 17.745L18 16.255L13.01 11.255ZM7.01001 11.255C4.52001 11.255 2.51001 9.245 2.51001 6.755C2.51001 4.265 4.52001 2.255 7.01001 2.255C9.50001 2.255 11.51 4.265 11.51 6.755C11.51 9.245 9.50001 11.255 7.01001 11.255Z' fill='%23ABB8C4'/%3E%3C/svg%3E%0A`}
               />
             </div>
             <NavLink to="/">Home</NavLink>
