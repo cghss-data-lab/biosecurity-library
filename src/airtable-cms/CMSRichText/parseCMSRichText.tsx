bcc614691260779a21c3e4c0f48165ece542e20f
Ryan <rzimmerman@talusanalytics.com>
2022-03-01 21:25:40 -0700

    making the cms.RichText parsing not sanitize HTML

--- src/airtable-cms/CMSRichText/parseCMSRichText.tsx
+++ src/airtable-cms/CMSRichText/parseCMSRichText.tsx
@@ -5,6 +5,8 @@ import html from 'remark-html'
 // takes a string of markdown and HTML from an airtable
 // rich text field and returns a string of html
 const parseCMSRichText = (string: string) =>
-  String(unified().use(markdown).use(html).processSync(string))
+  String(
+    unified().use(markdown).use(html, { sanitize: false }).processSync(string)
+  )
 
 export default parseCMSRichText
