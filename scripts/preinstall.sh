37c0b3c8cbb216e7e773fcd6c187375f6e952dc3
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2022-04-14 16:51:15 +0000

    Preinstall and postinstall

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,3 +1,12 @@
-echo "HELLO I'M AN INSTALL SCRIPT"
+echo "Installing Talus-Analytics gatsby-source-airtable fork"
+# yarn unlink "gatsby-source-airtable"
+cd gatsby-source-airtable;
+git submodule init;
+git submodule update;
+# yarn unlink; 
+yarn link; 
+cd ..;
+yarn link "gatsby-source-airtable"
+echo "Add talus-analytics registry"
+npm config set '@talus-analytics:registry' https://node.bit.dev
 
-yarn;
