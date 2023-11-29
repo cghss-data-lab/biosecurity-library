b22b369980c3b3defed0b4aa8d24c115992b874a
ryan-talus <rzimmerman@talusanalytics.com>
2023-11-29 13:27:44 -0300

    Revert "Use module import for network"
    This reverts commit 77095ea4ba5149513d29fc69e1f4185610a733c6.

--- src/components/detailpage/ResourceMapSection/ResourceMap/ResourceMap.tsx
+++ src/components/detailpage/ResourceMapSection/ResourceMap/ResourceMap.tsx
@@ -16,7 +16,8 @@ import {
   useAllCMSIcons,
   replaceFill,
 } from '@talus-analytics/library.airtable.cms-icon'
-import * as network from '@talus-analytics/viz.charts.network'
+import * as network from '../../../../network'
+// import * as network from '@talus-analytics/viz.charts.network'
 import {
   SettingsContext,
   defaultSettings,
