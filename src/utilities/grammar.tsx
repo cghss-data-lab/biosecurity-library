62918186cd3341fe29650be4f1e9037929d2fac8
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-01-23 21:19:18 +0000

    Adding grammar helper which handles any list of react elements, adding links to org names in the explore page

--- src/utilities/grammar.tsx
+++ src/utilities/grammar.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export const commaSeparatedList = (strings: string[]) => {
   if (!strings) return undefined
   if (strings.length === 1) return strings[0]
@@ -5,4 +7,21 @@ export const commaSeparatedList = (strings: string[]) => {
   const last = strings[strings.length - 1]
   const oxfordComma = strings.length > 2 ? ',' : ''
   return `${list}${oxfordComma} and ${last}`
+}
+
+export const commaSeparatedElements = (elements: React.ReactNode[]) => {
+  if (!elements) return undefined
+  if (elements.length === 1) return elements[0]
+  const withCommas: React.ReactNode[] = []
+  for (const element of elements.slice(0, -1)) {
+    withCommas.push(element)
+    withCommas.push(<>, </>)
+  }
+  withCommas.pop()
+  const oxfordComma = elements.length > 2
+  if (oxfordComma) withCommas.push(<>, </>)
+  const last = elements[elements.length - 1]
+  withCommas.push(<> and </>)
+  withCommas.push(last)
+  return withCommas
 }
